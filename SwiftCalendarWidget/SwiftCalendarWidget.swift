e7cd3f8012875040a205c5a49ce51f214da93144
Simon Berner <sibebzh@gmail.com>
2023-01-19 09:52:53 +0100

    feat: :boom: add circular lock screen widget (#2)

--- SwiftCalendarWidget/SwiftCalendarWidget.swift
+++ SwiftCalendarWidget/SwiftCalendarWidget.swift
@@ -64,7 +64,80 @@ struct CalendarEntry: TimelineEntry {
 }
 
 struct SwiftCalendarWidgetEntryView : View {
+    @Environment(\.widgetFamily) var family
     var entry: CalendarEntry
+
+    var body: some View {
+        switch family {
+            case .systemMedium:
+                MediumCalendarView(entry: entry, streakValue: calculateStreakDays())
+            case .accessoryCircular:
+                LockScreenCircularView(entry: entry)
+            case .accessoryRectangular:
+                Text("Test")
+            case .accessoryInline:
+                Label("Streak - \(calculateStreakDays())", systemImage: "swift")
+            case .systemSmall, .systemLarge, .systemExtraLarge: // don't supported here
+                EmptyView()
+            @unknown default:
+                EmptyView()
+        }
+    }
+
+    // TODO: Refactor this out
+    private func calculateStreakDays() -> Int {
+        guard !entry.days.isEmpty else { return 0 }
+
+        // Only consider already past days of the month from today
+        let nonFutureDays = entry.days.filter { $0.date!.dayInt <= Date().dayInt }
+
+        var streakCount = 0
+
+        // Reverse to get the days from today backwards for the streak
+        for day in nonFutureDays.reversed() {
+            if day.didStudy {
+                streakCount += 1
+            } else {
+                // exclude today from finishing the streak,
+                // because we might not have studied yet (and want to do so)
+                if day.date?.dayInt != Date().dayInt {
+                    break
+                }
+            }
+        }
+
+        return streakCount
+    }
+}
+
+struct SwiftCalendarWidget: Widget {
+    let kind: String = "SwiftCalendarWidget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            SwiftCalendarWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Swift Study Calendar")
+        .description("Track days you study in a row with streaks.")
+        .supportedFamilies([.systemMedium,
+                            .accessoryCircular,
+                            .accessoryRectangular,
+                            .accessoryInline])
+    }
+}
+
+struct SwiftCalendarWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftCalendarWidgetEntryView(entry: CalendarEntry(date: Date(), days: [], configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .accessoryCircular))
+    }
+}
+
+// MARK: - UI Components for widget class
+
+private struct MediumCalendarView: View {
+    var entry: CalendarEntry
+    var streakValue: Int
     let columns = Array(repeating: GridItem(.flexible()), count: 7)
 
     var body: some View {
@@ -72,7 +145,7 @@ struct SwiftCalendarWidgetEntryView : View {
             // Deep linking from the Widget to the StreakView
             Link(destination: URL(string: "streak")!) {
                 VStack {
-                    Text("\(calculateStreakDays())")
+                    Text("\(streakValue)")
                         .font(.system(size: 70, design: .rounded))
                         .bold()
                         .foregroundColor(.orange)
@@ -114,49 +187,31 @@ struct SwiftCalendarWidgetEntryView : View {
         }
         .padding()
     }
-
-    // TODO: Refactor this out
-    private func calculateStreakDays() -> Int {
-        guard !entry.days.isEmpty else { return 0 }
-
-        // Only consider already past days of the month from today
-        let nonFutureDays = entry.days.filter { $0.date!.dayInt <= Date().dayInt }
-
-        var streakCount = 0
-
-        // Reverse to get the days from today backwards for the streak
-        for day in nonFutureDays.reversed() {
-            if day.didStudy {
-                streakCount += 1
-            } else {
-                // exclude today from finishing the streak,
-                // because we might not have studied yet (and want to do so)
-                if day.date?.dayInt != Date().dayInt {
-                    break
-                }
-            }
-        }
-
-        return streakCount
-    }
 }
 
-struct SwiftCalendarWidget: Widget {
-    let kind: String = "SwiftCalendarWidget"
-
-    var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            SwiftCalendarWidgetEntryView(entry: entry)
-        }
-        .configurationDisplayName("Swift Study Calendar")
-        .description("Track days you study in a row with streaks.")
-        .supportedFamilies([.systemMedium])
+// Shows the total study days of the current month
+private struct LockScreenCircularView: View {
+    var entry: CalendarEntry
+    var currentCalendarDays: Int {
+        entry.days.filter { $0.date?.monthInt == Date().monthInt }.count
     }
-}
 
-struct SwiftCalendarWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftCalendarWidgetEntryView(entry: CalendarEntry(date: Date(), days: [], configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemMedium))
+    var daysStudied: Int {
+        entry.days.filter { $0.date?.monthInt == Date().monthInt }
+            .filter { $0.didStudy }.count
+    }
+
+    var body: some View {
+        Gauge(value: 15.0, in: 1...Double(currentCalendarDays)) {
+//            Image(systemName: "swift")
+        } currentValueLabel: {
+            Text("\(daysStudied)")
+        } minimumValueLabel: {
+            Text("0")
+        } maximumValueLabel: {
+            Text("\(currentCalendarDays)")
+        }
+        .gaugeStyle(.accessoryCircular)
+
     }
 }
